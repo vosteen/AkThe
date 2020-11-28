5e6d1609528b6a395fa77790ccf41921e98253bf
tlenfers <tlenfers@stud.hs-heilbronn.de>
2020-11-28 13:34:16 +0100

    added mpc_binary_and implementation

--- src/sign_sum.py
+++ src/sign_sum.py
@@ -1,4 +1,7 @@
+from mpyc.runtime import mpc
+
 from itertools import permutations
+
 
 class SignSum:
     def __init__(self):
@@ -7,8 +10,11 @@ class SignSum:
     def permutation(self, n, l):
         return [list(i) for i in list(permutations(range(5), 2)) if sorted(i) == list(i)]
 
-    def mpc_binary_and(self, permuations: list) -> bool:
-        pass
+    def mpc_binary_and(self, perm: list) -> bool:
+        bin_and = 1
+        for i in perm:
+            bin_and = mpc.and_(bin_and, i)
+        return bin_and
 
     def sum(self, n, permutations):
         pass
