5624d03461c61b939278289b11bfb798ffeaccdf
Lars Vosteen <lv@refr.vosteen.it>
2020-11-28 13:27:10 +0100

    added method to find permutations

--- src/sign_sum.py
+++ src/sign_sum.py
@@ -1,10 +1,14 @@
-class SignSum:
-   def __init__(self):
-       pass
-   def permutation(self,n):
-       pass
-   def mpc_binary_and(self,permuations:list)-> bool:
-       pass
-   def sum(self,n,permutations):
-       pass
+from itertools import permutations
 
+class SignSum:
+    def __init__(self):
+        pass
+
+    def permutation(self, n, l):
+        return [list(i) for i in list(permutations(range(5), 2)) if sorted(i) == list(i)]
+
+    def mpc_binary_and(self, permuations: list) -> bool:
+        pass
+
+    def sum(self, n, permutations):
+        pass
