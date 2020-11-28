f7a723cf71178562bb3faab82011acec418d2dc7
Lars Vosteen <lv@refr.vosteen.it>
2020-11-28 18:13:23 +0100

    dynamic failure

--- src/sign_sum.py
+++ src/sign_sum.py
@@ -1,4 +1,5 @@
 from mpyc.runtime import mpc
+from mpyc.seclists import seclist
 from itertools import permutations
 
 
@@ -6,48 +7,46 @@ def permutation(n, l):
     return [list(i) for i in list(permutations(range(n), l)) if sorted(i) == list(i)]
 
 
-async def mpc_prod(position: list):
+async def mpc_prod(values, position, n):
     await mpc.start()
 
-    if mpc.pid == 0:
-        vec0 = [1]
-    if mpc.pid == 1:
-        vec1 = [0]
-    if mpc.pid == 2:
-        vec2 = [1]
+    print(mpc.pid)
+
+    vec = [None]*n
+    for i in range(n):
+        if mpc.pid == i:
+            vec[i] = values[i]
 
     secint = mpc.SecInt(42)
     result_type = [secint()]
+    sec_vec = seclist([None]*n, secint)
 
-    sec_vec0 = mpc.input([secint(v) for v in vec0] if mpc.pid == 0 else result_type, senders=0)
-    sec_vec1 = mpc.input([secint(v) for v in vec1] if mpc.pid == 1 else result_type, senders=1)
-    sec_vec2 = mpc.input([secint(v) for v in vec2] if mpc.pid == 2 else result_type, senders=2)
 
-    if position[0] == 0:
-        if position[1] == 1:
-            t = [sec_vec0, sec_vec1]
-        else:
-            t = [sec_vec0, sec_vec2]
-    else:
-        t = [sec_vec1, sec_vec2]
+    for i in range(n):
+        if mpc.pid == i:
+            sec_vec[i] = mpc.input([secint(v) for v in [vec[i]]] if mpc.pid == i else result_type, senders=0)
+
+
+    t = []
+    for element in position:
+        print(sec_vec[element])
+        t.append(sec_vec[element])
 
     erg = mpc.in_prod(*t)
 
     result = await mpc.output(erg)
     print(result)
-
     await mpc.shutdown()
-
     return result
 
 
 async def main():
     n = 3  # number of participants
-
+    values = [1, 0, 1]
     permutations = permutation(n, int(n / 2) + 1)
     erg = -1
     for element in permutations:
-        temp = await mpc_prod(element)
+        temp = await mpc_prod(values, element, n)
         if temp > 0:
             erg = 1
             # break
