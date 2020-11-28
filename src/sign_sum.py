d7e96df8f91223c785e08b60f6f711c982f70d1f
tlenfers <tlenfers@stud.hs-heilbronn.de>
2020-11-28 13:05:28 +0100

    added SignSum class

---
+++ src/sign_sum.py
@@ -0,0 +1,10 @@
+class SignSum:
+   def __init__(self):
+       pass
+   def permutation(self,n):
+       pass
+   def mpc_binary_and(self,permuations:list)-> bool:
+       pass
+   def sum(self,n,permutations):
+       pass
+
